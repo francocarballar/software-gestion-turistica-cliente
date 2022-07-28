4fcfa52100bc6b8df2ce57890b1d796bfa49c4d9
francocarballar <francocarballar@gmail.com>
2022-07-28 17:27:45 -0300

    Fix deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Head from 'next/head'
-import Script from 'next/script'
+// import Script from 'next/script'
 import '../styles/globals.css'
 import { NavBar } from '../src/components/NavBar'
 import { Settings } from '../src/components/Settings'
@@ -11,11 +11,11 @@ function MyApp ({ Component, pageProps }) {
     <>
       <Head>
         <meta charset='UTF-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta name='robots' content='index, follow' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
         <link
           href='https://fonts.googleapis.com/css2?family=Anton&family=Roboto+Condensed:wght@400;700&display=swap'
           rel='stylesheet'
